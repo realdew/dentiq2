b4129d95b8d1f159821034d57a37e5ffda0d90e3
Ju-hyeon Lee <leejuhyeon@gmail.com>
2018-04-22 14:36:02 +0900

    TEST COMMENT

--- src/main/java/dentiq2/api/DentiqApiServer.java
+++ src/main/java/dentiq2/api/DentiqApiServer.java
@@ -3,6 +3,12 @@ package dentiq2.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * DentiqApiServer
+ *
+ * @author Ju-hyeon Lee (leejuhyeon@gmail.com)
+ * @date   2018-04-22
+ */
 @SpringBootApplication
 public class DentiqApiServer {
 
